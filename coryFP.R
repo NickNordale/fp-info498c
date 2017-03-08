b924e3bf9f482d78eed8cc6a9872457214275676
Cory Brown <cory_s_brown@msn.com>
2017-03-07 18:47:46 -0800

    trucking along

--- coryFP.R
+++ coryFP.R
@@ -22,21 +22,26 @@ ui <- fluidPage(
   # Application title
   titlePanel("ACA Impact across United States"),
   h2("Intro"),
-  p("The purpose of our research project is to help people better understand the impact of the Affordable Care 
-Act (Obamacare) on health insurance for United States citizens and analyze how future health law policy can change
-health coverage for many. Since Obamacare was signed into law in 2010, millions of people have benefitted that 
-wouldn't otherwise be able to. Many of these people were unemployed or had low paying jobs. Some couldn't afford 
-health insurance due to pre-existing health conditions. Now, more than 16 million Americans have obtained health 
-insurance coverage  which is more than ever before. Given the wider range of health care options, more people can 
-afford healthcare in the United States. With this said, we want to analyze data that allows us to seek insight into 
-how the changes of health law (existing and future) impacts the lives of most Americans."),
+  p("The purpose of this visualization is to share different insight using a compilation of State Data on 
+    the Affordable Care Act deriving from ASPE  and the Department of Health & Human Services. With this dataset, there 
+    are valuable answers to questions that many people are unaware of. There is alot of untapped knowledge to be gained 
+    and this interactive tool helps explore those potentials."),
   br(),
-  p("This map below introduces the uninsured rates of change by state before and after the ACA was implemented. 
-    As you can see, the states with a lighter blue shade have a lower rate of change for the amount of people uninsured 
-    between 2010 and 2015. The darker blue states have the highest rates of change for uninsured people. It seems that 
-    the amount of people uninsured living in the west coast has decreased the most in the country with Oregon, California 
-    and Nevada with substantial differences of almost a 10% decrease. The midwestern states including North Dakota, 
-    South Dakota, Wyoming and Nebrasksa have had the smallest uninsured rate change at around 2%. With 8 different rate 
+  p("Thes map-based visualization below shows uninsured rates for 2010, 2015 and the uninsured rate change between the two 
+    periods. From viewing the rate in 2010, Nevada and Texas have the highest uninsured rates in the US at around 20%. 
+    This means that roughly 20% of the people living in Nevada and Texas are without insurance. The Midwestern states, 
+    specifically Wisconsin and Minneosota, have the lowest rate of people without insurance in the United States. In 2015, 
+    the uninsured rate for each and every state lowered. It may be difficult to tell because the scale changes between time 
+    periods. Oklahoma, Texas and Georgia have the highest uninsured rates in 2015 and have the same color shade as the highest
+    rates in 2010 so you really need to pay attention to the scale. If you switch between the two time periods, you can
+    estimate manually but its too time consuming and isnt ideal for conducting data analysis."), 
+  br(),
+  p("So to really understand the difference between the two periods, the third filter to show rate of change is essential to
+    understanding the implications of the ACA. As you can see, the states with a lighter blue shade have a lower rate of change 
+    for the amount of people uninsured between 2010 and 2015. The darker blue states have the highest rates of change for uninsured 
+    people. It seems that the amount of people uninsured living in the west coast has decreased the most in the country with 
+    Oregon, California and Nevada with substantial differences of almost a 10% decrease. The midwestern states including North 
+    Dakota, South Dakota, Wyoming and Nebrasksa have had the smallest uninsured rate change at around 2%. With 8 different rate 
     change levels, it is very intuitive to understand the rates of change among each state. To say the least, I think this 
     visualization proves to help people better understand the impact of the ACA in a geographical sense. This heat map 
     displaying rate of change is an effective means of doing so."),
@@ -44,10 +49,9 @@ how the changes of health law (existing and future) impacts the lives of most Am
     sidebarPanel(
       selectInput(inputId = "columns",
                   label = "Explore:",
-                  choices = c("Uninsured Rate 2010", "Uninsured Rate 2015", "Uninsured Rate Change (2010-2015)"),
+                  choices = c("Uninsured Rate 2010", "Uninsured Rate 2015", "Uninsured Rate Change (2010-2015)", ""),
                   selected= "Uninsured Rate 2010")
     ),
-    
     # Show a plot of the generated distribution
     mainPanel(
       plotOutput("rolePlot")
