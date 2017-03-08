509ca4f18850c95897b4313614a0b3ff40c75ebc
NickNordale <nicknordale@gmail.com>
2017-03-08 08:07:21 -0800

    more shiny stuff

--- nnShiny/app.R
+++ nnShiny/app.R
@@ -33,16 +33,45 @@ tot$diff <- tot$rate - tot$value
 state.level <- tot %>% group_by(region) %>% 
   summarise(avg_aca = mean(value), avg_private = mean(rate), diff = mean(rate) - mean(value))
 
+state.age <- tot %>% group_by(region, age) %>% 
+  summarise(avg_aca = mean(value), avg_private = mean(rate), diff = mean(rate) - mean(value))
+
 state.level.map <- full_join(state.level, us, by = 'region')
 
 ui <- fluidPage(
   
-  selectInput(inputId = "type",
-              label = "Heath Insurance Type (aca, private",
-              choices = c('ACA', 'Private'),
-              selected = 'ACA'),
-  
-  plotOutput(outputId = "main_plot", width = '800px', height = '400px')
+  mainPanel(
+    fluidRow(
+      selectInput(inputId = "type",
+                  label = "Heath Insurance Type",
+                  choices = c('ACA', 'Private'),
+                  selected = 'ACA'),
+      
+      plotOutput(outputId = "main_plot", width = '800px', height = '400px')
+    ),
+    
+    fluidRow(
+      column(
+        6,
+        selectInput(inputId = "table.age",
+                    label = "Age",
+                    choices = c('All', 'Child', '21', '27', '30', '40', '50', '60'),
+                    selected = 'All'),
+        
+        tableOutput('top.10.diff')
+      ),
+      column(
+        6,
+        selectInput(inputId = "table.age2",
+                    label = "Age",
+                    choices = c('All', 'Child', '21', '27', '30', '40', '50', '60'),
+                    selected = 'All'),
+        
+        tableOutput('bottom.10.diff')
+      )
+    ),
+    width = 10
+  )
 )
 
 server <- function(input, output) {
@@ -54,6 +83,74 @@ server <- function(input, output) {
     } else if (input$type == 'Private') {
       metric = 'avg_private'
     }
+    
+    if (input$table.age == 'All') {
+      ages = c(10, 21, 27, 30, 40, 50, 60)
+    } else if (input$table.age == 'Child') {
+      ages = c(10)
+    } else if (input$table.age == '21-26') {
+      ages = c(21)
+    } else if (input$table.age == '27-29') {
+      ages = c(27)
+    } else if (input$table.age == '30-39') {
+      ages = c(30)
+    } else if (input$table.age == '40-49') {
+      ages = c(40)
+    } else if (input$table.age == '50-59') {
+      ages = c(50)
+    } else if (input$table.age == '60+') {
+      ages = c(60)
+    }
+    
+    if (input$table.age2 == 'All') {
+      ages = c(10, 21, 27, 30, 40, 50, 60)
+    } else if (input$table.age == 'Child') {
+      ages = c(10)
+    } else if (input$table.age == '21-26') {
+      ages = c(21)
+    } else if (input$table.age == '27-29') {
+      ages = c(27)
+    } else if (input$table.age == '30-39') {
+      ages = c(30)
+    } else if (input$table.age == '40-49') {
+      ages = c(40)
+    } else if (input$table.age == '50-59') {
+      ages = c(50)
+    } else if (input$table.age == '60+') {
+      ages = c(60)
+    }
+    
+    output$top.10.diff <- renderTable(
+      state.age %>% 
+        filter(age %in% ages) %>%
+        group_by(region) %>% 
+        summarise(avg_aca = mean(avg_aca), avg_private = mean(avg_private), diff = mean(avg_private) - mean(avg_aca)) %>%
+        arrange(-diff) %>% 
+        top_n(10) %>% 
+        mutate('State' = sub("(.)", "\\U\\1", region, perl=TRUE), 'Avg ACA Savings' = round(diff, 2)) %>% 
+        mutate('Avg ACA Savings' = paste0('$', `Avg ACA Savings`)) %>%
+        select(one_of(c('State', 'Avg ACA Savings'))),
+      spacing = 'm', 
+      striped = TRUE,  
+      width = '100%', 
+      align = 'c'
+    )
+    
+    output$bottom.10.diff <- renderTable(
+      state.age %>% 
+        filter(age %in% ages) %>%
+        group_by(region) %>% 
+        summarise(avg_aca = mean(avg_aca), avg_private = mean(avg_private), diff = mean(avg_private) - mean(avg_aca)) %>%
+        arrange(diff) %>% 
+        top_n(-10) %>% 
+        mutate('State' = sub("(.)", "\\U\\1", region, perl=TRUE), 'Avg ACA Savings' = round(diff, 2)) %>% 
+        mutate('Avg ACA Savings' = paste0('$', `Avg ACA Savings`)) %>%
+        select(one_of(c('State', 'Avg ACA Savings'))),
+      spacing = 'm', 
+      striped = TRUE,  
+      width = '100%', 
+      align = 'c'
+    )
     
     ggplot(state.level.map, aes(map_id = region)) + 
       geom_map(aes_string(fill = metric), map = us) + 
